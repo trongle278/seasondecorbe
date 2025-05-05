daa486a0ad29059da0efc8c6c2be16da7dbea425
Trong Le <trongle278@gmail.com>
2025-05-05 18:57:59 +0700

    Forget to add migration to push

---
+++ DataAccessLayer/Migrations/20250505115356_AddIsQuoted.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessObject.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsQuoted : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "MeetingId",
+                table: "ZoomMeetings",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsQuoted",
+                table: "Bookings",
+                type: "bit",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsQuoted",
+                table: "Bookings");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MeetingId",
+                table: "ZoomMeetings",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
